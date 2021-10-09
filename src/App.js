88b8474a33b0155285618085f12e6868cf87d67c
ABdevs29 <abhishek.elmoro@gmail.com>
2021-10-09 11:22:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import MiniDrawer from './Drawer';
 
 function App() {
+
+  //Sample local data
+  const tasks = [
+    {
+      taskName: "Get Groceries",
+      taskGroup: "shopping",
+      date: "2021-09-30",
+      priority: "low",
+      isCompleted: false
+    },
+    {
+      taskName: "Vaccination 2nd dose",
+      taskGroup: "health",
+      date: "2021-09-24",
+      priority: "high",
+      isCompleted: false
+    },
+    {
+      taskName: "Purchase mobile for sister in Amazon Sale",
+      taskGroup: "shopping",
+      date: "2021-10-12",
+      priority: "medium",
+      isCompleted: false
+    },
+    {
+    taskName: "Find good charity fund",
+    taskGroup: "general",
+    date: "2021-09-29",
+    priority: "low",
+    isCompleted: false
+    }
+  ];
+
   return (
+    //Main component
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MiniDrawer tasks={tasks}/>
     </div>
   );
 }
